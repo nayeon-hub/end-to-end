ba0d48e852e65cfd6fb629d52ed7ee7d7847990c
Park Na Yeon <within197@naver.com>
2021-07-20 20:41:41 +0900

    퍼즐 랜덤 섞기 및 보여주기

--- slide-puzzle/madePuzzle.js
+++ slide-puzzle/madePuzzle.js
@@ -1,8 +1,12 @@
-const startBtn = document.querySelector(".puzzle-control");
+const controlBox = document.querySelector(".puzzle-control");
+const startBtn = controlBox.querySelector("button");
+const puzzleList = document.querySelectorAll(".puzzle-piece");
+
 const answer = [1, 2, 3, 4, 5, 6, 7, 8];
+let li = [];
 
 function madeQus() {
-  let li = [];
+  li = [];
   while (li.length < 8) {
     const n = Math.floor(Math.random() * 8) + 1;
     const check = li.indexOf(n);
@@ -10,6 +14,27 @@ function madeQus() {
       li.push(n);
     }
   }
+  li = [li.slice(0, 3), li.slice(3, 6), li.slice(6, 8)];
+  console.log(li);
   return li;
 }
-console.log(madeQus());
+
+function showPuzzle(p) {
+  let x = 0;
+  for (let i = 0; i < 3; i++) {
+    for (let j = 0; j < 3; j++) {
+      if (li[i][j]) {
+        puzzleList[x].innerHTML = li[i][j];
+      }
+      x += 1;
+    }
+  }
+}
+
+function gameStart() {
+  console.log("game start!");
+  const puzzle = madeQus();
+  showPuzzle(puzzle);
+}
+
+startBtn.addEventListener("click", gameStart);
